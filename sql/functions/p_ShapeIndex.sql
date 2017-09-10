d47c03bf06bcedae06beb8290a66b1990326b110
Andrea Rosado <andrearosado93@gmail.com>
2017-09-10 20:50:45 +0200

    updated name function

--- sql/functions/p_ShapeIndex.sql
+++ sql/functions/p_ShapeIndex.sql
@@ -7,11 +7,11 @@ Shape Index - devuelve el perímetro (m) del polígono dividido por la raíz cua
 WITH  patches (geom,categ) AS (VALUES
                                (ST_GeomFromText('POLYGON((0 0,0 1,1 1,1 0,0 0))',25830),'Urbano'))
 
-SELECT lm.p_shape(geom) As p_shape FROM patches;
+SELECT lm.p_shapeindex(geom) As p_shape FROM patches;
 */
 
 
-CREATE OR REPLACE FUNCTION lm.p_shape(geom geometry)
+CREATE OR REPLACE FUNCTION lm.p_shapeindex(geom geometry)
 RETURNS double precision AS 
 $$
 
@@ -22,4 +22,4 @@ LANGUAGE SQL
 IMMUTABLE
 RETURNS NULL ON NULL INPUT;
 
-COMMENT ON FUNCTION lm.p_shape(geom geometry) IS 'Divide el perímetro por la raíz cuadrada del área del polígono ajustado por una constante de estándar cuadrático.';
+COMMENT ON FUNCTION lm.p_shapeindex(geom geometry) IS 'Divide el perímetro por la raíz cuadrada del área del polígono ajustado por una constante de estándar cuadrático.';
