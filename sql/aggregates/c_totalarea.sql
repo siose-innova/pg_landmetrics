c1c99cf1ae2d9e8af6fa568d92007ac56044b9fe
benizar <benizar@gmail.com>
2017-10-16 15:10:45 +0200

    Excluded from extension

--- sql/aggregates/c_totalarea.sql
+++ sql/aggregates/c_totalarea.sql
@@ -8,37 +8,35 @@ SELECT c_totalarea_state(ARRAY[('Agricultural area',('Total Class Area'::text, 1
 */
 
 CREATE OR REPLACE FUNCTION c_totalarea_state(
-	current_state labeled_metric[],
+	current_state metric_labeled,
 	geom geometry,
 	category text)
-    RETURNS labeled_metric[]
+    RETURNS metric_labeled
     LANGUAGE 'sql'
 
 AS 
 $BODY$
 
 WITH inputs AS (
-    SELECT current_state AS cstate
+    	SELECT current_state AS cstate
 ), melt AS (
-    SELECT unnest(cstate) AS m2 FROM inputs
-    UNION 
-    SELECT (category,('Total Class Area'::text, (p_area(geom)).value, 'Ha.'::text))::labeled_metric AS m2
+    	SELECT unnest((cstate).pairs) AS m2 FROM inputs
+    	UNION 
+	SELECT (category, (p_area(geom)).value)::metric_labeled_pair AS m2
 ), summarize AS (
-    SELECT (m2).label, ((m2).metric).name, SUM(((m2).metric).value) AS value, ((m2).metric).units  FROM melt GROUP BY (m2).label, ((m2).metric).name, ((m2).metric).units
+	SELECT (m2).label, SUM((m2).value) AS value FROM melt GROUP BY (m2).label
 )
-SELECT ARRAY(SELECT (label::text, (name, value, units))::labeled_metric FROM summarize);
+SELECT (2, ARRAY(SELECT (label, value)::metric_labeled_pair FROM summarize))::metric_labeled;
 
 $BODY$;
-
-
 
 
 -- SAMPLE USAGE
 -- SELECT c_totalarea(geom,category) FROM sample_patches_25830;
 CREATE AGGREGATE c_totalarea(geometry, text)(
     SFUNC=c_totalarea_state,
-    STYPE=labeled_metric[],
-    INITCOND='{}'
+    STYPE=metric_labeled,
+    INITCOND='(2,{})'
 );
 
 COMMENT ON AGGREGATE c_totalarea(geom geometry, category text) IS 'Calcula la suma de las áreas de los polígonos de la misma categoría dividido por 10.000 para devolver un valor en Hectáreas.';
